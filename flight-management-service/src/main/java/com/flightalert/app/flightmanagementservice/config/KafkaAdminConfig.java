709fc50de3b3878c11034eca9d0cfd044a53a065
jorge.vega <jorge.vega@sofka.com.co>
2025-03-06 10:53:36 -0500

    feat: add Kafka configuration for flight management service

---
+++ flight-management-service/src/main/java/com/flightalert/app/flightmanagementservice/config/KafkaAdminConfig.java
@@ -0,0 +1,37 @@
+package com.flightalert.app.flightmanagementservice.config;
+
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+
+@Configuration
+public class KafkaAdminConfig {
+
+    private final KafkaProperties kafkaProperties;
+
+    public KafkaAdminConfig(KafkaProperties kafkaProperties) {
+        this.kafkaProperties = kafkaProperties;
+    }
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        var configs = new HashMap<String, Object>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public KafkaAdmin.NewTopics topics() {
+        return new KafkaAdmin.NewTopics(
+                TopicBuilder.name("mcsv-flight-management-service").partitions(2).replicas(2).build()
+        );
+    }
+
+
+}
